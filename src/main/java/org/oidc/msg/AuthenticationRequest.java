52f6dd2c73912557e814e5ce963272e3eab01507
janne lauros <janne.lauros@gmail.com>
2018-07-14 15:50:25 +0300

    huge duke nuke commit

--- src/main/java/org/oidc/msg/AuthenticationRequest.java
+++ src/main/java/org/oidc/msg/AuthenticationRequest.java
@@ -1,6 +1,5 @@
 package org.oidc.msg;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,24 +21,10 @@ public class AuthenticationRequest extends AuthorizationRequest {
    *          client_id(REQUIRED), redirect_uri(REQUIRED), scope (REQUIRED), nonce
    *          (OPTIONAL/REQUIRED), state(RECOMMENDED) and other claims described in
    *          http://openid.net/specs/openid-connect-core-1_0.html#AuthRequest.
-   * @throws InvalidClaimException
-   *           if response_type claim invalid
    */
-  @SuppressWarnings("unchecked")
-  public AuthenticationRequest(Map<String, Object> claims) throws InvalidClaimException {
+  public AuthenticationRequest(Map<String, Object> claims) {
     super(claims);
-    /*
-    requiredClaims.add("redirect_uri");
-    requiredClaims.add("scope");
-    // verify response type claim individually as it needs to be used.
-    ClaimsValidator.validate("response_type", claims.get("response_type"), fetchMessageType());
-    // if response type implies implicit flow, nonce is mandatory
-    if (((List<String>) claims.get("response_type")).contains("id_token")
-        && !((List<String>) claims.get("response_type")).contains("code")) {
-      requiredClaims.add("nonce");
     
-    }
-    */
   }
 
 }
