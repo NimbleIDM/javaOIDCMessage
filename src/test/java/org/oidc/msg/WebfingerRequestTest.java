60573d40e5b9856ee5b4d55f13bee11a18962a0a
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-07-16 11:50:37 +0300

    Initial version

---
+++ src/test/java/org/oidc/msg/WebfingerRequestTest.java
@@ -0,0 +1,44 @@
+package org.oidc.msg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link WebfingerRequest}.
+ */
+public class WebfingerRequestTest {
+  
+  @Test
+  public void testDefaultRelParameter() throws InvalidClaimException {
+    Map<String, Object> claims = new HashMap<String, Object>();
+    claims.put("resource", "value");
+    WebfingerRequest req = new WebfingerRequest();
+    req.addClaim("resource", "value");
+    Map<String, Object> msgClaims = req.getClaims();
+    Assert.assertEquals("value", msgClaims.get("resource"));
+    Assert.assertEquals("http://openid.net/specs/connect/1.0/issuer", msgClaims.get("rel"));
+  }
+
+  @Test
+  public void testSuccessMandatoryParameters() throws InvalidClaimException {
+    Map<String, Object> claims = new HashMap<String, Object>();
+    claims.put("resource", "value");
+    claims.put("rel", "relValue");
+    WebfingerRequest req = new WebfingerRequest(claims);
+    Map<String, Object> msgClaims = req.getClaims();
+    Assert.assertEquals("value", msgClaims.get("resource"));
+    Assert.assertEquals("relValue", msgClaims.get("rel"));
+  }
+  
+  @Test(expected = InvalidClaimException.class)
+  public void testFailureMissingResponseTypeMandatoryParameters() throws InvalidClaimException {
+    Map<String, Object> claims = new HashMap<String, Object>();
+    claims.put("custom", "value");
+    WebfingerRequest req = new WebfingerRequest(claims);
+    req.getClaims(); // triggers verify()
+  }
+
+}
