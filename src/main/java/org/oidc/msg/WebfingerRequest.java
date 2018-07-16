84622d3c94cf89d0284cb64fb008a5939cd6866f
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-07-16 11:50:19 +0300

    Renaming + parameter configuration

--- src/main/java/org/oidc/msg/WebfingerRequest.java
+++ src/main/java/org/oidc/msg/WebfingerRequest.java
@@ -1,20 +1,23 @@
 package org.oidc.msg;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class WebfingerRequestMessage extends AbstractMessage {
+public class WebfingerRequest extends AbstractMessage {
   
   { //Set parameter requirements for message.
     paramVerDefs.put("resource",
         ParameterVerification.SINGLE_REQUIRED_STRING.getValue());
     paramVerDefs.put("rel", ParameterVerification.SINGLE_REQUIRED_STRING.getValue());
-  }
-  
-  public WebfingerRequestMessage() {
-    super();
+    defaultValues.put("rel", "http://openid.net/specs/connect/1.0/issuer");
   }
 
-  public WebfingerRequestMessage(Map<String, Object> claims) {
+  public WebfingerRequest() {
+    this(new HashMap<String, Object>());
+  }
+
+  public WebfingerRequest(Map<String, Object> claims) {
     super(claims);
+    addDefaultValues();
   }
 }
