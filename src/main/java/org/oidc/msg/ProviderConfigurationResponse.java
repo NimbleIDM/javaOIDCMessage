31e702c20f5ed11dc824a9b8817969bb434091b7
Henri Mikkonen <henri.mikkonen@iki.fi>
2018-07-17 17:52:33 +0300

    Added more verification

--- src/main/java/org/oidc/msg/ProviderConfigurationResponse.java
+++ src/main/java/org/oidc/msg/ProviderConfigurationResponse.java
@@ -1,94 +1,92 @@
 package org.oidc.msg;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class ProviderConfigurationResponse extends AbstractMessage {
-  
-  { //Set parameter requirements for message.
+public class ProviderConfigurationResponse extends org.oidc.msg.oauth2.ASConfigurationResponse {
+
+  { // Remove requirements from OAuth2 ASConfigurationResponse
+    paramVerDefs.clear();
+    // Set parameter requirements for message.
     paramVerDefs.put("issuer", ParameterVerification.SINGLE_REQUIRED_STRING.getValue());
-    paramVerDefs.put("authorization_endpoint", 
+    paramVerDefs.put("authorization_endpoint",
         ParameterVerification.SINGLE_REQUIRED_STRING.getValue());
-    paramVerDefs.put("token_endpoint", 
-        ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
-    paramVerDefs.put("userinfo_endpoint", 
-        ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
+    paramVerDefs.put("token_endpoint", ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
+    paramVerDefs.put("userinfo_endpoint", ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
     paramVerDefs.put("jwks_uri", ParameterVerification.SINGLE_REQUIRED_STRING.getValue());
-    paramVerDefs.put("registration_endpoint", 
+    paramVerDefs.put("registration_endpoint",
         ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
-    paramVerDefs.put("scopes_supported", 
-        ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("response_types_supported", 
+    paramVerDefs.put("scopes_supported", ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
+    paramVerDefs.put("response_types_supported",
         ParameterVerification.REQUIRED_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("response_modes_supported", 
+    paramVerDefs.put("response_modes_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("grant_types_supported", 
+    paramVerDefs.put("grant_types_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("acr_values_supported", 
+    paramVerDefs.put("acr_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("subject_types_supported", 
+    paramVerDefs.put("subject_types_supported",
         ParameterVerification.REQUIRED_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("id_token_signing_alg_values_supported", 
+    paramVerDefs.put("id_token_signing_alg_values_supported",
         ParameterVerification.REQUIRED_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("id_token_encryption_alg_values_supported", 
+    paramVerDefs.put("id_token_encryption_alg_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("id_token_encryption_enc_values_supported", 
+    paramVerDefs.put("id_token_encryption_enc_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("userinfo_signing_alg_values_supported", 
+    paramVerDefs.put("userinfo_signing_alg_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("userinfo_encryption_alg_values_supported", 
+    paramVerDefs.put("userinfo_encryption_alg_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("userinfo_encryption_enc_values_supported", 
+    paramVerDefs.put("userinfo_encryption_enc_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("request_object_signing_alg_values_supported", 
+    paramVerDefs.put("request_object_signing_alg_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("request_object_encryption_alg_values_supported", 
+    paramVerDefs.put("request_object_encryption_alg_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("request_object_encryption_enc_values_supported", 
+    paramVerDefs.put("request_object_encryption_enc_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("token_endpoint_auth_methods_supported", 
+    paramVerDefs.put("token_endpoint_auth_methods_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("token_endpoint_auth_signing_alg_values_supported", 
+    paramVerDefs.put("token_endpoint_auth_signing_alg_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("display_values_supported", 
+    paramVerDefs.put("display_values_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("claim_types_supported", 
+    paramVerDefs.put("claim_types_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("claims_supported", 
-        ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("service_documentation", 
+    paramVerDefs.put("claims_supported", ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
+    paramVerDefs.put("service_documentation",
         ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
-    paramVerDefs.put("claims_locales_supported", 
+    paramVerDefs.put("claims_locales_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("ui_locales_supported", 
+    paramVerDefs.put("ui_locales_supported",
         ParameterVerification.OPTIONAL_LIST_OF_STRINGS.getValue());
-    paramVerDefs.put("claims_parameter_supported", 
+    paramVerDefs.put("claims_parameter_supported",
         ParameterVerification.SINGLE_OPTIONAL_BOOLEAN.getValue());
-    paramVerDefs.put("request_parameter_supported", 
+    paramVerDefs.put("request_parameter_supported",
         ParameterVerification.SINGLE_OPTIONAL_BOOLEAN.getValue());
-    paramVerDefs.put("request_uri_parameter_supported", 
+    paramVerDefs.put("request_uri_parameter_supported",
         ParameterVerification.SINGLE_OPTIONAL_BOOLEAN.getValue());
-    paramVerDefs.put("require_request_uri_registration", 
+    paramVerDefs.put("require_request_uri_registration",
         ParameterVerification.SINGLE_OPTIONAL_BOOLEAN.getValue());
-    paramVerDefs.put("op_policy_uri", 
+    paramVerDefs.put("op_policy_uri", ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
+    paramVerDefs.put("op_tos_uri", ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
+    paramVerDefs.put("check_session_iframe",
         ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
-    paramVerDefs.put("op_tos_uri", 
+    paramVerDefs.put("end_session_endpoint",
         ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
-    paramVerDefs.put("check_session_iframe", 
-        ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
-    paramVerDefs.put("end_session_endpoint", 
-        ParameterVerification.SINGLE_OPTIONAL_STRING.getValue());
-    
+
     defaultValues.put("version", "3.0");
-    defaultValues.put("token_endpoint_auth_methods_supported", 
+    defaultValues.put("token_endpoint_auth_methods_supported",
         Arrays.asList("client_secret_basic"));
     defaultValues.put("claims_parameter_supported", Boolean.FALSE);
     defaultValues.put("request_parameter_supported", Boolean.FALSE);
     defaultValues.put("request_uri_parameter_supported", Boolean.TRUE);
     defaultValues.put("require_request_uri_registration", Boolean.TRUE);
-    defaultValues.put("grant_types_supported", 
-        Arrays.asList("authorization_code", "implicit"));
+    defaultValues.put("grant_types_supported", Arrays.asList("authorization_code", "implicit"));
   }
 
   public ProviderConfigurationResponse() {
@@ -98,5 +96,47 @@ public class ProviderConfigurationResponse extends AbstractMessage {
   public ProviderConfigurationResponse(Map<String, Object> claims) {
     super(claims);
     addDefaultValues();
+  }
+
+  @Override
+  public boolean verify() throws InvalidClaimException {
+    boolean verify = false;
+    try {
+      verify = super.verify();
+    } catch (InvalidClaimException e) {
+      // carry on, possibly populate more error messages
+    }
+    if (getClaims().containsKey("scopes_supported")) {
+      @SuppressWarnings("unchecked")
+      List<String> scopes = (List<String>) getClaims().get("scopes_supported");
+      if (!scopes.contains("openid")) {
+        error.getMessages()
+            .add("Parameter 'scopes_supported' does not contain expected value 'openid'");
+      }
+    }
+    try {
+      URI uri = new URI((String)getClaims().get("issuer"));
+      if (!"https".equals(uri.getScheme()) || uri.getQuery() != null || uri.getFragment() != null) {
+        error.getMessages().add("Parameter 'issuer' has an invalid value");
+      }
+    } catch (URISyntaxException e) {
+      error.getMessages().add("Parameter 'issuer' is not a valid URL");
+    }
+    @SuppressWarnings("unchecked")
+    List<String> rts = (List<String>) getClaims().get("response_types_supported");
+    for (String rt : rts) {
+      if (rt.contains("code")) {
+        if (!getClaims().containsKey("token_endpoint")) {
+          error.getMessages().add("'token_endpoint' is required when code response_type is supported");
+        }
+      }
+    }
+
+    if (error.getMessages().size() > 0) {
+      throw new InvalidClaimException(
+          "Message parameter verification failed. See Error object for details");
+    }
+
+    return verify;
   }
 }
